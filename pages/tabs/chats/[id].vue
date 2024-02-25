b01eefad7d4e8a360584f042aaa2a540d4b6c2fe
Semolik <70706334+Semolik@users.noreply.github.com>
2024-02-25 17:19:17 +0700

    added chats infinite scroll(with bugs)

--- pages/tabs/chats/[id].vue
+++ pages/tabs/chats/[id].vue
@@ -1,5 +1,5 @@
 <template>
-    <ion-page class="page-chat-messages">
+    <ion-page class="page-chat-messages" fullscreen>
         <div class="head">
             <nuxt-link class="back" to="/tabs/chats">
                 <Icon name="material-symbols:arrow-back-ios-rounded" />
@@ -10,23 +10,29 @@
             </div>
             <avatar :image="toUser.avatar" class="avatar" />
         </div>
-        <div class="wrapper">
-            <div class="messages">
+        <ion-content>
+            <ion-infinite-scroll
+                @ionInfinite="ionInfinite"
+                position="top"
+                :disabled="is_end"
+            >
+                <ion-infinite-scroll-content
+                    class="scorll-animaton"
+                ></ion-infinite-scroll-content>
+            </ion-infinite-scroll>
+            <ion-list class="messages" ref="messagesContainer">
                 <chats-message
                     :message="message"
                     v-for="message in messages"
                     :key="message.id"
                     :from-me="message.from_user.id === userData.id"
                 />
-            </div>
-            <div class="send-message">
-                <auth-input
-                    v-model="newMessage"
-                    placeholder="Введите сообщение"
-                />
-                <div class="send">
-                    <Icon name="material-symbols:send" @click="sendMessage" />
-                </div>
+            </ion-list>
+        </ion-content>
+        <div class="send-message">
+            <auth-input v-model="newMessage" placeholder="Введите сообщение" />
+            <div class="send">
+                <Icon name="material-symbols:send" @click="sendMessage" />
             </div>
         </div>
     </ion-page>
@@ -37,15 +43,22 @@ import { useAuthStore } from "~/stores/auth";
 const { userData } = useAuthStore();
 const router = useRouter();
 const { id } = router.currentRoute.value.params;
+const { wsURL } = useRuntimeConfig().public;
+const messagesContainer = ref(null);
+const scrollToBottom = () => {
+    messagesContainer.value.scrollTop = messagesContainer.value.scrollHeight;
+};
+// onMounted(scrollToBottom);
 const newMessage = ref("");
-
 const chat = await ChatsService.getChatChatsChatIdGet(id);
 const toUser = getUserToId(userData.id, chat.user_1, chat.user_2);
 const page = ref(1);
 const messages = ref(
-    await ChatsService.getMessagesChatsChatIdMessagesGet(id, page.value)
+    (
+        await ChatsService.getMessagesChatsChatIdMessagesGet(id, page.value)
+    ).reverse()
 );
-const { wsURL } = useRuntimeConfig().public;
+
 const { disconnect } = useWebsocket(
     `${wsURL}/chats/${id}/messages/ws`,
     (event) => {
@@ -56,10 +69,26 @@ const { disconnect } = useWebsocket(
     }
 );
 watch(router.currentRoute, disconnect);
-const sending = ref(false);
+const is_end = ref(false);
+const ionInfinite = async (ev) => {
+    if (is_end.value) {
+        ev.target.complete();
+        return;
+    }
+    const new_messages = await ChatsService.getMessagesChatsChatIdMessagesGet(
+        id,
+        ++page.value
+    );
+    if (new_messages.length === 0) {
+        is_end.value = true;
+        ev.target.complete();
+        return;
+    }
+    messages.value = [...new_messages.reverse(), ...messages.value];
+    ev.target.complete();
+};
 const sendMessage = async () => {
-    if (!newMessage.value || sending.value) return;
-    sending.value = true;
+    if (!newMessage.value) return;
     messages.value.push(
         await ChatsService.sendMessageChatsChatIdMessagesPost(
             id,
@@ -67,12 +96,15 @@ const sendMessage = async () => {
         )
     );
     newMessage.value = "";
-    sending.value = false;
+    scrollToBottom();
 };
 </script>
 <style scoped lang="scss">
-.page-chat-messages {
+.scorll-animaton {
     background-color: $secondary;
+}
+.page-chat-messages {
+    --overflow: hidden;
     .head {
         display: flex;
         background-color: white;
@@ -95,66 +127,64 @@ const sendMessage = async () => {
             }
         }
     }
-    .wrapper {
-        padding: 10px;
+
+    .messages {
         display: flex;
         flex-direction: column;
         gap: 10px;
+        background-color: $secondary;
+        padding: 10px;
+    }
 
-        .messages {
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-            flex-grow: 1;
+    .input {
+        box-sizing: border-box;
+        flex-basis: 4rem;
+        flex-shrink: 0;
+        display: flex;
+        align-items: center;
+        padding: 0 0.5rem 0 1.5rem;
+
+        i {
+            font-size: 1.5rem;
+            margin-right: 1rem;
+
+            cursor: pointer;
+            transition: color 200ms;
         }
 
-        .input {
-            box-sizing: border-box;
-            flex-basis: 4rem;
-            flex-shrink: 0;
-            display: flex;
-            align-items: center;
-            padding: 0 0.5rem 0 1.5rem;
+        input {
+            border: none;
+            background-image: none;
+            background-color: white;
+            padding: 0.5rem 1rem;
+            margin-right: 1rem;
+            border-radius: 1.125rem;
+            flex-grow: 2;
+            box-shadow: 0 0 1rem rgba(black, 0.1),
+                0rem 1rem 1rem -1rem rgba(black, 0.2);
 
-            i {
-                font-size: 1.5rem;
-                margin-right: 1rem;
-
-                cursor: pointer;
-                transition: color 200ms;
-            }
-
-            input {
-                border: none;
-                background-image: none;
-                background-color: white;
-                padding: 0.5rem 1rem;
-                margin-right: 1rem;
-                border-radius: 1.125rem;
-                flex-grow: 2;
-                box-shadow: 0 0 1rem rgba(black, 0.1),
-                    0rem 1rem 1rem -1rem rgba(black, 0.2);
-
-                font-family: Red hat Display, sans-serif;
-                font-weight: 400;
-                letter-spacing: 0.025em;
-            }
+            font-family: Red hat Display, sans-serif;
+            font-weight: 400;
+            letter-spacing: 0.025em;
         }
-        .send-message {
-            display: grid;
-            grid-template-columns: 1fr auto;
-            gap: 10px;
-            .send {
-                @include flex-center;
-                background-color: $primary;
-                border-radius: 50%;
-                width: 40px;
-                height: 40px;
-                svg {
-                    width: 15px;
-                    height: 15px;
-                    color: white;
-                }
+    }
+    .send-message {
+        display: grid;
+        grid-template-columns: 1fr auto;
+        gap: 10px;
+        padding: 10px;
+        padding-top: 5px;
+        border-top: 1px solid $quaternary;
+        .send {
+            @include flex-center;
+            background-color: $primary;
+            border-radius: 50%;
+            width: 40px;
+            height: 40px;
+            svg {
+                width: 15px;
+                height: 15px;
+                color: white;
             }
         }
     }
