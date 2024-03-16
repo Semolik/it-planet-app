79e86e69ed7d3b2647c2b963527e2ae4d591e595
Qislord <AnanadKryt@gmail.com>
2024-03-16 18:09:21 +0700

    add email update modal

--- components/GreenButton.vue
+++ components/GreenButton.vue
@@ -1,22 +1,35 @@
 <template>
-    <button class="btn">
+    <button :disabled="disabled" class="btn">
         <slot></slot>
     </button>
 </template>
 
 <script setup>
-
+defineProps ({
+    disabled:{
+        required:false,
+        default:false,
+    },
+    padding:{
+        required:false,
+        default: '5px',
+    }
+})
 </script>
 
 <style scoped lang="scss">
 .btn {
-    padding: 5px;
+    padding: v-bind(padding);
     border-radius: 8px;
     background-color: #62A87C;
     user-select: none;
 
     &:active {
         background-color: #56946d;
+    }
+    &:disabled{
+        background-color: $light-primary;
+        color: $tertiary-text;
     }
 }
 </style>
