53eb51c658b614111621c5de10b5d3627d0be773
Semolik <70706334+Semolik@users.noreply.github.com>
2024-04-10 15:39:32 +0700

    Update api-url.js

--- plugins/api-url.js
+++ plugins/api-url.js
@@ -1,5 +1,5 @@
 import { OpenAPI } from "@/client";
 export default defineNuxtPlugin((nuxtApp) => {
-    OpenAPI.BASE = process.dev ? "/api" : "https://api.frienda.website";
+    OpenAPI.BASE = process.dev ? "/api" : "https://frienda.website";
     OpenAPI.WITH_CREDENTIALS = true;
 });
